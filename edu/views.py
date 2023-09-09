f9b3baca9d5ae53880d0c16fe5167e363084dcfa
kdhwow0829 <kdh640829@gmail.com>
2023-09-09 14:53:21 +0900

    09/09 tag를 배웠다

--- edu/views.py
+++ edu/views.py
@@ -6,3 +6,10 @@ class Index(View):
 
     def get(self, request):
         return render(request, self.template_name)
+    
+
+class TagStudy(View):
+    template_name = 'tag_study.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
