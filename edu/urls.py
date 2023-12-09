d6790e3de43ff3ca8aef45655a43e72775072933
kdhwow0829 <kdh640829@gmail.com>
2023-12-09 14:49:40 +0900

    2023/12/9/토

--- edu/urls.py
+++ edu/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('tag', views.TagStudy.as_view(), name='tag_study'),
     path('new', views.NewContent.as_view(), name="new_content"),
     path('survey', views.Survey.as_view(), name="survey"),
+    path('feed/<int:pk>', views.FeedDetail.as_view(), name='feed_detail'),
 ]
--- edu/views.py
+++ edu/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import View
+from django.views.generic import View, DetailView
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Feed
 class Index(View):
@@ -47,4 +47,14 @@ class Survey(View):
 
     def get(self, request):
         return render(request, self.template_name)
-    
+
+class FeedDetail(DetailView):
+    model = Feed
+    template_name ="feed/detail.html"
+
+    def get_context_data(self,**kwargs) :
+        context = super().get_context_data(**kwargs)
+        feed = get_object_or_404(Feed, pk = self.kwargs['pk'])
+        context['feed'] = feed
+
+        return context
